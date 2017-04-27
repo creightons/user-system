e393b84cc715b83e8d646beec2578dd64f3200a5
a <creightons26@gmail.com>
2017-04-26 23:46:09 -0400

    Added some working templates, updated the server, got the datbase working

--- server.py
+++ server.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
+
+path = os.path.dirname( os.path.realpath(__file__) )
+database_path = os.path.join(path, 'mydb.sqlite')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + database_path
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 @app.route('/')
 def index():
-	return "Welcome to the main page"
+#	return "Welcome to the main page"
+	return render_template('index.html')
