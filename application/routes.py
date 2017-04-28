3313eeb80345db0b5bb455a6a0908aa07b7f4d64
a <creightons26@gmail.com>
2017-04-27 22:09:26 -0400

    Separated the route handlers into a separate file

---
+++ application/routes.py
@@ -0,0 +1,20 @@
+from flask import request, render_template
+from models.user import User
+from database import db
+
+def apply_routes(app):
+	@app.route('/', methods=['GET', 'POST'])
+	def index():
+	#	return "Welcome to the main page"
+		if request.method == 'POST':
+			username = request.form['username']
+			password = request.form['password']
+			new_user = User(username, password)
+
+			db.session.add(new_user)
+			db.session.commit()
+
+		users = User.query.all()
+		usernames = [ user.username for user in users ]
+
+		return render_template('index.html', usernames=usernames)
--- application/server.py
+++ application/server.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask
 import os
-
 app = Flask(__name__)
 
 path = os.path.dirname( os.path.realpath(__file__) )
@@ -13,21 +12,5 @@ from database import db
 # Initialize Database
 db.init_app(app)
 
-# Import models after database is initialized
-from models.user import User
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-#	return "Welcome to the main page"
-	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
-		new_user = User(username, password)
-
-		db.session.add(new_user)
-		db.session.commit()
-
-	users = User.query.all()
-	usernames = [ user.username for user in users ]
-
-	return render_template('index.html', usernames=usernames)
+from routes import apply_routes
+apply_routes(app)
