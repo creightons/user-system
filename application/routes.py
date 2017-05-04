6e9c138cbe6e53d1e51c65ddd01346f24ef66374
Creighton <creightons26@gmail.com>
2017-05-04 19:03:20 -0400

    Updated user profile page to show permissions and have form to change user permissions

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,12 @@
-from flask import request, render_template
-from models import User
+from __future__ import print_function # Print goes to STDERR
+import sys
+from flask import request, render_template, redirect
+from models import User, Permission
 from database import db
+
+# Print data to STDERR in Flask terminal
+def pr(arg):
+	print(arg, file=sys.stderr)
 
 def apply_routes(app):
 	@app.route('/', methods=['GET', 'POST'])
@@ -22,15 +28,31 @@ def apply_routes(app):
 
 	@app.route('/user_profile/<user_id>')
 	def user_route(user_id):
-		
 		user = User.query.filter(User.id == user_id).first()
 
-		user_permissions = [ permission.description for permission in user.permissions ]
+		permissions = Permission.query.all()
+
+		user_permission_map = { permission.id : True for permission in user.permissions }
+
+		permission_context = []
+		for permission in permissions:
+			permission_context.append({
+				'id': permission.id,
+				'description': permission.description,
+				'user_has_permission': permission.id in user_permission_map,
+			})
 
 		context = {
 			'username': user.username,
-			'id': user.id,
-			'permissions': user_permissions,
+			'user_id': user.id,
+			'permissions': permission_context,
 		}
 
 		return render_template('user_profile.html', context=context)
+
+	@app.route('/permissions', methods=['POST'])
+	def add_permissions():
+		data = request.form
+		redirect_url = '/user_profile/' + data['user_id']
+		pr(data)
+		return redirect(redirect_url, code=200)
